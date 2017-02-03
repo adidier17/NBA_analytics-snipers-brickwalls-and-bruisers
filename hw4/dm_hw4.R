c78fcd6c5775a51d19e9b56164290beaa50d3d47
ethen8181 <ethen8181@gmail.com>
2017-02-02 18:21:44 -0600

    hw4 report

--- hw4/dm_hw4.R
+++ hw4/dm_hw4.R
@@ -2,16 +2,23 @@
 library(psych)
 ###1
 music<-read.csv("music.csv")
-
 fit1<-factanal(music[,-28],factors=2,score="reg",rotation="varimax")
-fit1 #31, 47 maybe, 49 maybe, 53, 54
+fit1$loadings #31, 47 maybe, 49 maybe, 53, 54
 
 #musicpca<-prcomp(music[,-28],scale=T)
 #musicpca$rotation #31, 49
 
-alpha(music[,-28],check.keys=TRUE) #31, 34, 43, 45, 49
-
+music_alpha <- alpha(music[,-28],check.keys=TRUE) #31, 34, 43, 45, 49
+print(music_alpha$alpha.drop)
 ##all three methods suggest removing 31 and 49, also removing 54 because of promiscuity
+
+# compute alpha for each scale
+music1<-music[,c(1,2,3,5,6,7,16,19,20,23,25)]
+music2<-music[,c(8,9,10,11,12,13,14,15,17,18,21,24)]
+music1_alpha <- alpha(music1)
+music2_alpha <- alpha(music2)
+print(music1_alpha$alpha.drop)
+print(music2_alpha$alpha.drop)
 
 fit2<-factanal(music[,-c(4,22,27,28)],factors=2,score="reg",rotation="varimax")
 fit2loadings<-fit2$loadings
@@ -19,12 +26,6 @@ fit2loadings[fit2loadings<.3]<-0
 fit2loadings
 #Factor 1 is social aspects of music. Includes questions 28, 29, 30, 32, 33, 34, 43, 46, 47, 50, 52
 #Factor 2 is personal aspects. Includes questions 35, 36, 37, 38, 39, 40, 41, 42, 44, 45, 48, 51
-
-music1<-music[,c(1,2,3,5,6,7,16,19,20,23,25)]
-music2<-music[,c(8,9,10,11,12,13,14,15,17,18,21,24)]
-
-alpha(music1)
-alpha(music2)
 
 ###2
 book<-read.csv("book.csv")
@@ -37,8 +38,8 @@ fit3loadings[fit3loadings<.3]<-0
 fit3loadings #fiction, legends, philosophy, religion*, psychology*, music*, facsimilie, politics, maps, gamesriddles, sports
 #videos, nonbooks
 
-exclude<-c("fiction1", "legends6", "philosophy7", "facsimile17", "politics22", "maps30", "GamesRiddles38", "sports39",
-           "videos50", "nonbooks99","cartoons5", "railroads27")
+exclude<-c("fiction1", "legends6", "philosophy7", "facsimile17", "politics22", "maps30", 
+		   "GamesRiddles38", "sports39", "videos50", "nonbooks99","cartoons5", "railroads27")
 
 alpha(book,check.keys = TRUE) #remove sports
 bookpca$rotation #sports
@@ -48,11 +49,4 @@ fit4$loadings
 #factor 1 is sciences
 #factor 2 is arts/humanities
 #factor 3 is hobbies
-
-
-
-
-
-
-
 
